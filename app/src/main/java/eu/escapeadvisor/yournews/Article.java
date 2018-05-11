679dfd8fc007bc6435d1073d6327e5d03341ecc6
Melsara <kaxhortz@gmail.com>
2018-05-11 19:58:15 +0200

    Article with thumbnail.

--- app/src/main/java/eu/escapeadvisor/yournews/Article.java
+++ app/src/main/java/eu/escapeadvisor/yournews/Article.java
@@ -1,12 +1,16 @@
 package eu.escapeadvisor.yournews;
 
-public class Article {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Article implements Parcelable {
 
     private String mSection;
     private String mTitle;
     private String mAuthor;
     private String mDate;
     private String mWebUrl;
+    private String mThumbnail;
 
     public Article (String section, String title, String webUrl, String author, String date) {
         mSection = section;
@@ -15,6 +19,32 @@ public class Article {
         mAuthor = author;
         mDate = date;
     }
+
+    public Article (String section, String title, String webUrl, String author, String date, String thumbnail) {
+        mSection = section;
+        mTitle = title;
+        mWebUrl = webUrl;
+        mAuthor = author;
+        mDate = date;
+        mThumbnail = thumbnail;
+    }
+
+    protected Article(Parcel in) {
+        mThumbnail = in.readString();
+        mTitle = in.readString();
+    }
+
+    public static final Creator<Article> CREATOR = new Creator<Article>() {
+        @Override
+        public Article createFromParcel(Parcel in) {
+            return new Article(in);
+        }
+
+        @Override
+        public Article[] newArray(int size) {
+            return new Article[size];
+        }
+    };
 
     public String getSection () {
         return mSection;
@@ -36,8 +66,23 @@ public class Article {
         return mDate;
     }
 
-    public void setAuthor(String author) {
-        this.mAuthor = author;
+    public String getThumbnail() {
+        return mThumbnail;
+    }
+
+    public void setThumbnail(String mThumbnail) {
+        this.mThumbnail = mThumbnail;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(mThumbnail);
+
     }
 }
 
